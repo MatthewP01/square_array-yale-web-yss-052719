a35b411c2c29651db28f9f9513386df5213ca024
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-18 17:31:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,8 @@
 def square_array(array)
   # your code here
   array = [1, 2, 3, 4, 5]
-  ran_arr = array(1..5)
-  new_arr = []
-  for i in ran_arr
-    sq = i*i
-    new_arr.append(sq)
-  end
+  array.each do |square|
+    square = square * 2
 
   # array.each do |square|
 
